a0b779d79e54f1626c858e7ae66e4db174d628b5
Jumi_hsu <scm80507211@gmail.com>
2019-01-23 18:47:03 +0800

    [ch7]開始練習建置web，學習MVC模式

---
+++ chapter7_web/webMVC_noteFirst.py
@@ -0,0 +1,61 @@
+
+# 上層路徑設定
+bottomPathHome = r"D:\GIT_Tortoise_Jumi_NB\HeadFirst_Python"
+bottomPathOffice = r"C:\Users\Jumi_Hsu\Desktop\TortoiseGit_Jumi_jfi\HeadFirst_python"
+
+# 檔案夾位置設定
+btmPath = bottomPathOffice
+fileFolder = r"\chapter7_web"
+
+# 匯入modual
+import os
+import sys
+import pickle  # 需要讀檔+輸出檔案
+sys.path.append(btmPath + "/openTxt")
+sys.path.append(btmPath + "/athleteList")
+import openTxt
+import athleteList
+
+# 變更工作路徑
+openTxt.changechdir(btmPath, fileFolder)
+
+
+
+print("----------- 定義 預計會常用到的的關鍵字 ----------")
+nameFourPlayers = ["james", "julie", "mikey", "sarah"]
+# nameFourPlayers[0]+".txt"
+print("nameFourPlayers =", nameFourPlayers, "\n")
+
+
+# 使用 import 的 module：athleteList
+# 一次只要處理一個 name 就好
+
+
+'''
+丟進4個名字
+讀取那4個名字的txt
+用pickle存起來
+存成一個長度=4的，以名字為key的字典
+'''
+# def put_to_store(nameList):
+
+
+    # return a
+
+'''
+讀取檔案 (用pickle)
+輸出的時候輸出成 一個長度=4，以名字為key，其值為athleteList class物件的字典
+'''
+# def get_from_store(b):
+#     return b
+
+for each in nameFourPlayers:
+    rank = 3
+    locals()["%s" % (each) +
+             "OriData"] = athleteList.get_coach_data(each+".txt")
+    locals()["%s" % (each) + "TOP3"] = locals()["%s" %
+                                                (each) + "OriData"].TOP(rank)
+
+    print(each+"OriData =", locals()["%s" % (each) + "OriData"])
+    print(locals()["%s" % (each) + "OriData"].name, "TOP3 =",  # 改成直接用 obj.name去呼叫
+          locals()["%s" % (each) + "TOP3"], "\n")
